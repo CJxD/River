b91ef76940de35fe59dac7c675bfbe6546b92cda
Chris Watts <Chris@CJWatts.com>
2014-03-05 11:35:26 +0000

    Fix math library

--- lib/math.ml
+++ lib/math.ml
@@ -8,7 +8,7 @@ class math =
 			| Int x, Float y -> Float (float_of_int x +. y)
 			| Float x, Int y -> Float (x +. float_of_int y)
 			| Float x, Float y -> Float (x +. y)
-			| _, _ -> raise (Failure "Fucking idiot.");;
+			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
 
 		method minus x y = 
 			match x, y with
@@ -16,7 +16,7 @@ class math =
 			| Int x, Float y -> Float (float_of_int x -. y)
 			| Float x, Int y -> Float (a -. float_of_int y)
 			| Float x, Float y -> Float (a -. y)
-			| _, _ -> raise (Failure "Fucking idiot.");;
+			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
 
 		method times x y = 
 			match x, y with
@@ -24,7 +24,7 @@ class math =
 			| Int x, Float y -> Float (float_of_int x *. y)
 			| Float x, Int y -> Float (a *. float_of_int y)
 			| Float x, Float y -> Float (a *. y)
-			| _, _ -> raise (Failure "Fucking idiot.");;
+			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
 
 		method divide x y = 
 			match x, y with
@@ -32,7 +32,7 @@ class math =
 			| Int x, Float y -> Float (float_of_int x /. y)
 			| Float x, Int y -> Float (a /. float_of_int y)
 			| Float x, Float y -> Float (a /. y)
-			| _, _ -> raise (Failure "Fucking idiot.");;
+			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
 
 		method modulo x y = 
 			match x, y with
@@ -40,9 +40,9 @@ class math =
 			| Int x, Float y -> Float (mod_float (float_of_int x) y)
 			| Float x, Int y -> Float (mod_float x (float_of_int y))
 			| Float x, Float y -> Float (mod_float x y)
-			| _, _ -> raise (Failure "Fucking idiot.");;
+			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
 
-		method power y = function
+		method power x y =
 			match x, y with
 			| Int x, Int y -> (
 				match y with
@@ -56,9 +56,12 @@ class math =
 			| Int x, Float y -> Float (float_of_int x ** y)
 			| Float x, Int y -> Float (a ** float_of_int y)
 			| Float x, Float y -> Float (a ** y)
-			| _, _ -> raise (Failure "Fucking idiot.");;
+			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
 
-		method unary_minus x = 
-			-x
+		method unary_minus x =
+			match x with
+			| Int x -> -x
+			| Float x -> -x
+			| _ -> raise (Failure "you may only perform math operations on numeric types.")
 
 	end;;
