8cda921ecfb7d1b35ff68d3f976337d1196c36c3
CJxD <Chris@CJWatts.com>
2014-03-05 12:38:20 +0000

    Fix math lib

--- lib/math.ml
+++ lib/math.ml
@@ -1,4 +1,8 @@
 
+open Language
+
+exception Illegal_argument of string;;
+
 class math = 
 	object(this)
 
@@ -8,39 +12,39 @@ class math =
 			| Int x, Float y -> Float (float_of_int x +. y)
 			| Float x, Int y -> Float (x +. float_of_int y)
 			| Float x, Float y -> Float (x +. y)
-			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| _, _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 		method minus x y = 
 			match x, y with
-			| Int x, Int y -> Int (a - y)
+			| Int x, Int y -> Int (x - y)
 			| Int x, Float y -> Float (float_of_int x -. y)
-			| Float x, Int y -> Float (a -. float_of_int y)
-			| Float x, Float y -> Float (a -. y)
-			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| Float x, Int y -> Float (x -. float_of_int y)
+			| Float x, Float y -> Float (x -. y)
+			| _, _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 		method times x y = 
 			match x, y with
-			| Int x, Int y -> Int (a * y)
+			| Int x, Int y -> Int (x * y)
 			| Int x, Float y -> Float (float_of_int x *. y)
-			| Float x, Int y -> Float (a *. float_of_int y)
-			| Float x, Float y -> Float (a *. y)
-			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| Float x, Int y -> Float (x *. float_of_int y)
+			| Float x, Float y -> Float (x *. y)
+			| _, _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 		method divide x y = 
 			match x, y with
-			| Int x, Int y -> Int (a / y)
+			| Int x, Int y -> Int (x / y)
 			| Int x, Float y -> Float (float_of_int x /. y)
-			| Float x, Int y -> Float (a /. float_of_int y)
-			| Float x, Float y -> Float (a /. y)
-			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| Float x, Int y -> Float (x /. float_of_int y)
+			| Float x, Float y -> Float (x /. y)
+			| _, _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 		method modulo x y = 
 			match x, y with
-			| Int x, Int y -> Int (a mod y)
+			| Int x, Int y -> Int (x mod y)
 			| Int x, Float y -> Float (mod_float (float_of_int x) y)
 			| Float x, Int y -> Float (mod_float x (float_of_int y))
 			| Float x, Float y -> Float (mod_float x y)
-			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| _, _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 		method power x y =
 			match x, y with
@@ -49,19 +53,19 @@ class math =
 				| 0 -> Int 1
 				| 1 -> Int x
 				| y -> 
-					let n = this#power (Int x) (Int (b / 2)) in
+					let n = this#power (Int x) (Int (y / 2)) in
 						this#times
 							(this#times n n)
 							(if y mod 2 = 0 then (Int 1) else (Int x)))
 			| Int x, Float y -> Float (float_of_int x ** y)
-			| Float x, Int y -> Float (a ** float_of_int y)
-			| Float x, Float y -> Float (a ** y)
-			| _, _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| Float x, Int y -> Float (x ** float_of_int y)
+			| Float x, Float y -> Float (x ** y)
+			| _, _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 		method unary_minus x =
 			match x with
-			| Int x -> -x
-			| Float x -> -x
-			| _ -> raise (Failure "you may only perform math operations on numeric types.")
+			| Int x -> Int (0 - x)
+			| Float x -> Float (0. -. x)
+			| _ -> raise (Illegal_argument "you may only perform math operations on numeric types.")
 
 	end;;
